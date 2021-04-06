5806ae69d99be4364f156368a9f2ad1b636ead50
luodongyang <luodongyang@shizhuang-inc.com>
2021-04-06 22:12:32 +0800

    chore: add route

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
-
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { h } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+const routes = {
+  '/': HelloWorld,
+  '/home': Home,
+};
+
+const SimpleRouter = {
+  data: () => ({
+    currentRoute: window.location.pathname
+  }),
+
+  computed: {
+    CurrentComponent() {
+      return routes[this.currentRoute] || NotFoundComponent
+    }
+  },
+
+  render() {
+    return h(this.CurrentComponent)
   }
-})
+}
+export default SimpleRouter;
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
