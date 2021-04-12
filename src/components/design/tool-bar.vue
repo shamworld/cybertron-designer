366948ddf0b19bd1eb86ca6f5c27e635447caa26
luodongyang <luodongyang@shizhuang-inc.com>
2021-04-13 00:20:36 +0800

    chore: add side

--- src/components/design/tool-bar.vue
+++ src/components/design/tool-bar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-screen flex bg-white dark:bg-gray-800 h-40">
-    <div class="w-1/4 h-full text-white">
+    <div class="w-1/4 h-full text-black dark:text-white">
       左区
     </div>
-    <div class="w-1/2 h-full">
+    <div class="w-1/2 h-full text-black dark:text-white">
       中区
     </div>
-    <div class="w-1/4 h-full">
+    <div class="w-1/4 h-full text-black dark:text-white">
       右区
     </div>
   </div>
