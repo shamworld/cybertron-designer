91b98259a7c7be3010ec1cf9446fefe971c39ccb
luodongyang <luodongyang@shizhuang-inc.com>
2021-04-12 21:32:01 +0800

    chore: add more routes

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,26 @@
 import Home from '@/components/Home.vue';
 import DesignBase from '@/components/design/base.vue';
+import ManagementBase from '@/components/management/base.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    {
-        path: '/', name: 'Index', redirect: '/home',
-    }, {
-        path: '/home', name: 'Home', component: Home,
-    }, {
-        path: '/design', name: 'Design', component: DesignBase,
-    },
+  {
+    path: '/', name: 'Index', redirect: '/home',
+  },
+  {
+    path: '/home', name: 'Home', component: Home,
+  },
+  {
+    path: '/design', name: 'Design', component: DesignBase,
+  },
+  {
+    path: '/management', name: 'Management', component: ManagementBase, children: [],
+  },
 ];
 
 const router = createRouter({
-                                history: createWebHistory(),
-                                routes,
-                            });
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
