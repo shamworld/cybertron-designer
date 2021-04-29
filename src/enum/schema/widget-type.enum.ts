c65b6e373bca3b15703500a856496a697ad88985
luodongyang <luodongyang@shizhuang-inc.com>
2021-04-30 00:42:25 +0800

    chore: add schema enum and interface

---
+++ src/enum/schema/widget-type.enum.ts
@@ -0,0 +1,17 @@
+enum WidgetType {
+  container = 'container',
+  text = 'text',
+  link = 'link',
+  image = 'image',
+  input = 'input',
+  radio = 'radio',
+  checkbox = 'checkbox',
+  form = 'form',
+  list = 'list',
+  table = 'table',
+  tree = 'tree',
+  // 自定义类型——第三方组件，或者用户自己重构出来的组件，带源代码或者不带源代码
+  custom = 'custom'
+}
+
+export default WidgetType;
