42c5b83a7e2e0ce7f232ec3b2fc30768574a35c0
luodongyang <luodongyang@shizhuang-inc.com>
2021-05-08 16:45:33 +0800

    chore: remove display block

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,13 +2,11 @@ import StyleSchema from '@/interface/schema/style.schema';
 import DynamicObject from '@/interface/dynamic-object';
 
 export function convertSchemaToStyle(styleSchema: StyleSchema[]) {
-  const result = styleSchema.reduce(
+  return styleSchema.reduce(
     (accumulator: DynamicObject, curVal) => {
       accumulator[curVal.name] = `${curVal.value}${curVal.unit}`;
       return accumulator;
     },
     {}
   );
-  result.display = 'block';
-  return result;
 }
