6882bbd6c8aa0b776d4293eab003a1152db81162
luodongyang <luodongyang@shizhuang-inc.com>
2021-05-10 21:36:02 +0800

    chore: add widget suffix

--- src/enum/schema/widget-type.enum.ts
+++ src/enum/schema/widget-type.enum.ts
@@ -1,17 +1,17 @@
 enum WidgetType {
-  container = 'container',
-  text = 'txt',
-  link = 'link',
-  image = 'image',
-  input = 'input',
-  radio = 'radio',
-  checkbox = 'checkbox',
-  form = 'form',
-  list = 'list',
-  table = 'table',
-  tree = 'tree',
+  container = 'container-widget',
+  text = 'txt-widget',
+  link = 'link-widget',
+  image = 'image-widget',
+  input = 'input-widget',
+  radio = 'radio-widget',
+  checkbox = 'checkbox-widget',
+  form = 'form-widget',
+  list = 'list-widget',
+  table = 'table-widget',
+  tree = 'tree-widget',
   // 自定义类型——第三方组件，或者用户自己重构出来的组件，带源代码或者不带源代码
-  custom = 'custom'
+  custom = 'custom-widget'
 }
 
 export default WidgetType;
