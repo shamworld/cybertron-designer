b20be15beee3a3de5e4ed81da9bff5dad69a1a7c
luodongyang <luodongyang@shizhuang-inc.com>
2021-05-13 00:30:01 +0800

    chore: add margin of style unit

--- src/views/design/components/form-props/border-setting.vue
+++ src/views/design/components/form-props/border-setting.vue
@@ -1,33 +1,70 @@
 <template>
   <a-collapse :activeKey(v-model)="activeKey">
     <a-collapse-panel key="1" header="边框">
-
+      <a-form :label-col="labelCol" :model="layoutStyle" :wrapper-col="wrapperCol">
+        <a-form-item label="上边框">
+          <div class="flex flex-shrink-0">
+            <a-input v-model:value="layoutStyle.borderTop.value"></a-input>
+            <span class="mx-10"> {{ layoutStyle.borderTop.unit }}</span>
+          </div>
+        </a-form-item>
+        <a-form-item label="下边框">
+          <div class="flex flex-shrink-0">
+            <a-input v-model:value="layoutStyle.borderBottom.value"></a-input>
+            <span class="mx-10"> {{ layoutStyle.borderBottom.unit }}</span>
+          </div>
+        </a-form-item>
+        <a-form-item label="左边框">
+          <div class="flex flex-shrink-0">
+            <a-input v-model:value="layoutStyle.borderLeft.value"></a-input>
+            <span class="mx-10"> {{ layoutStyle.borderLeft.unit }}</span>
+          </div>
+        </a-form-item>
+        <a-form-item label="右边框">
+          <div class="flex flex-shrink-0">
+            <a-input v-model:value="layoutStyle.borderRight.value"></a-input>
+            <span class="mx-10"> {{ layoutStyle.borderRight.unit }}</span>
+          </div>
+        </a-form-item>
+      </a-form>
     </a-collapse-panel>
   </a-collapse>
 </template>
 
 <script>
+import StyleValueUnit from '@/enum/style-value-unit';
+
 export default {
   name: 'border-setting',
   data() {
     return {
       activeKey: '1',
-      style: {
+      labelCol: {
+        span: 8
+      },
+      wrapperCol: {
+        span: 12
+      },
+      layoutStyle: {
         borderTop: {
           type: String,
-          default: ''
+          value: 0,
+          unit: StyleValueUnit.px
         },
         borderRight: {
           type: String,
-          default: '',
+          value: 0,
+          unit: StyleValueUnit.px,
         },
         borderBottom: {
           type: String,
-          default: ''
+          value: 0,
+          unit: StyleValueUnit.px
         },
         borderLeft: {
           type: String,
-          default: ''
+          value: 0,
+          unit: StyleValueUnit.px
         },
       }
     }
--- src/views/design/components/form-props/layout-setting.vue
+++ src/views/design/components/form-props/layout-setting.vue
@@ -5,73 +5,73 @@
         <a-form-item label="宽度">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.width.value"></a-input>
-            <span> {{ layoutStyle.width.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.width.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="高度">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.height.value"></a-input>
-            <span> {{ layoutStyle.height.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.height.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="上外边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.marginTop.value"></a-input>
-            <span> {{ layoutStyle.marginTop.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.marginTop.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="下外边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.marginBottom.value"></a-input>
-            <span> {{ layoutStyle.marginBottom.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.marginBottom.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="左外边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.marginLeft.value"></a-input>
-            <span> {{ layoutStyle.marginLeft.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.marginLeft.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="右外边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.marginRight.value"></a-input>
-            <span> {{ layoutStyle.marginRight.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.marginRight.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="上内边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.paddingTop.value"></a-input>
-            <span> {{ layoutStyle.paddingTop.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.paddingTop.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="下内边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.paddingBottom.value"></a-input>
-            <span> {{ layoutStyle.paddingBottom.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.paddingBottom.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="左内边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.paddingLeft.value"></a-input>
-            <span> {{ layoutStyle.paddingLeft.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.paddingLeft.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="右内边距">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.paddingRight.value"></a-input>
-            <span> {{ layoutStyle.paddingRight.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.paddingRight.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="最小高度">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.minWidth.value"></a-input>
-            <span> {{ layoutStyle.minWidth.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.minWidth.unit }}</span>
           </div>
         </a-form-item>
         <a-form-item label="最大高度">
           <div class="flex flex-shrink-0">
             <a-input v-model:value="layoutStyle.minHeight.value"></a-input>
-            <span> {{ layoutStyle.minHeight.unit }}</span>
+            <span class="mx-10"> {{ layoutStyle.minHeight.unit }}</span>
           </div>
         </a-form-item>
       </a-form>
