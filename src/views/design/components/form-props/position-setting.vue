1d91e313ab1bb03349ab0721c685658f84b98d8e
luodongyang <luodongyang@shizhuang-inc.com>
2021-05-13 00:35:59 +0800

    chore: add positioning form

--- src/views/design/components/form-props/position-setting.vue
+++ src/views/design/components/form-props/position-setting.vue
@@ -1,17 +1,45 @@
 <template>
   <a-collapse :activeKey(v-model)="activeKey">
     <a-collapse-panel key="1" header="定位">
-
+      <a-form :label-col="labelCol" :model="layoutStyle" :wrapper-col="wrapperCol">
+        <a-form-item label="定位">
+          <div class="flex flex-shrink-0">
+            <a-select v-model:value="layoutStyle.position.value">
+              <a-select-option value="static">默认</a-select-option>
+              <a-select-option value="relative">相对定位</a-select-option>
+              <a-select-option value="absolute">绝对定位</a-select-option>
+              <a-select-option value="fixed">固定定位</a-select-option>
+              <a-select-option value="sticky">粘性（吸顶）定位</a-select-option>
+            </a-select>
+          </div>
+        </a-form-item>
+      </a-form>
     </a-collapse-panel>
   </a-collapse>
 </template>
 
 <script>
+import StyleValueUnit from '@/enum/style-value-unit';
+import Positioning from '@/enum/schema/positioning.enum';
+
 export default {
   name: 'position-setting',
   data() {
     return {
       activeKey: '1',
+      labelCol: {
+        span: 8
+      },
+      wrapperCol: {
+        span: 12
+      },
+      layoutStyle: {
+        position: {
+          type: Positioning,
+          value: Positioning.static,
+          unit: StyleValueUnit.none
+        },
+      }
     }
   }
 };
