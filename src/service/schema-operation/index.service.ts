51b8128766ca4edcf62890c23129a6edcaff4ac7
luodongyang <luodongyang@shizhuang-inc.com>
2021-05-27 00:17:47 +0800

    refactor: extract style object reference

--- src/service/schema-operation/index.service.ts
+++ src/service/schema-operation/index.service.ts
@@ -8,6 +8,188 @@ import StyleValueUnit from '@/enum/style-value-unit';
 import DynamicObject from '@/interface/dynamic-object';
 import Layout from '@/enum/layout';
 
+const borderStyle = {
+  borderTop: {
+    name: 'border-top',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  borderRight: {
+    name: 'border-right',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  borderBottom: {
+    name: 'border-bottom',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  borderLeft: {
+    name: 'border-left',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+};
+
+const visualEffectStyle = {
+  background: {
+    name: 'background',
+    value: '#fff',
+    unit: StyleValueUnit.none
+  },
+  boxShadow: {
+    name: 'box-shadow',
+    value: '',
+    unit: StyleValueUnit.none
+  },
+  transition: {
+    name: 'transition',
+    value: '',
+    unit: StyleValueUnit.none
+  },
+  animation: {
+    name: 'animation',
+    value: '',
+    unit: StyleValueUnit.none
+  },
+};
+
+const boxModelStyle = {
+  boxSizing: {
+    name: 'box-sizing',
+    value: 'border-box',
+    unit: StyleValueUnit.none
+  },
+  marginTop: {
+    name: 'margin-top',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  marginRight: {
+    name: 'margin-right',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  marginBottom: {
+    name: 'margin-bottom',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  marginLeft: {
+    name: 'margin-left',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  paddingTop: {
+    name: 'padding-top',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  paddingRight: {
+    name: 'padding-right',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  paddingBottom: {
+    name: 'padding-bottom',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  paddingLeft: {
+    name: 'padding-left',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  width: {
+    name: 'width',
+    value: 'auto',
+    unit: StyleValueUnit.px
+  },
+  height: {
+    name: 'height',
+    value: 'auto',
+    unit: StyleValueUnit.px
+  },
+  minWidth: {
+    name: 'min-width',
+    value: 'auto',
+    unit: StyleValueUnit.px
+  },
+  minHeight: {
+    name: 'min-height',
+    value: 40,
+    unit: StyleValueUnit.px
+  },
+};
+
+const positionStyle = {
+  position: {
+    name: 'position',
+    value: 'static',
+    unit: StyleValueUnit.none
+  },
+  top: {
+    name: 'top',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  right: {
+    name: 'right',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  bottom: {
+    name: 'bottom',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+  left: {
+    name: 'left',
+    value: 0,
+    unit: StyleValueUnit.px
+  },
+};
+
+const width = {
+  name: 'width',
+  value: 'auto',
+  unit: StyleValueUnit.none
+};
+
+const height = {
+  name: 'height',
+  value: 20,
+  unit: StyleValueUnit.px
+};
+
+const fontStyle = {
+  fontFamily: {
+    name: 'font-family',
+    value: 'Microsoft YaHei',
+    unit: StyleValueUnit.px,
+  },
+  fontSize: {
+    name: 'font-size',
+    value: 12,
+    unit: StyleValueUnit.px
+  },
+  fontWeight: {
+    name: 'font-weight',
+    value: 600,
+    unit: StyleValueUnit.none
+  },
+  lineHeight: {
+    name: 'line-height',
+    value: 20,
+    unit: StyleValueUnit.px
+  },
+  color: {
+    name: 'color',
+    value: '#000',
+    unit: StyleValueUnit.none
+  },
+};
+
 class SchemaService implements SchemaOperator {
   static widgetGenerationDict: DynamicObject = {
     'container-widget': (data: { type: any }) => {
@@ -23,116 +205,10 @@ class SchemaService implements SchemaOperator {
               value: Layout.block,
               unit: StyleValueUnit.none
             },
-            marginTop: {
-              name: 'margin-top',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            marginRight: {
-              name: 'margin-right',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            marginBottom: {
-              name: 'margin-bottom',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            marginLeft: {
-              name: 'margin-left',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            paddingTop: {
-              name: 'padding-top',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            paddingRight: {
-              name: 'padding-right',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            paddingBottom: {
-              name: 'padding-bottom',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            paddingLeft: {
-              name: 'padding-left',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            width: {
-              name: 'width',
-              value: 'auto',
-              unit: StyleValueUnit.px
-            },
-            height: {
-              name: 'height',
-              value: 'auto',
-              unit: StyleValueUnit.px
-            },
-            minWidth: {
-              name: 'min-width',
-              value: 'auto',
-              unit: StyleValueUnit.px
-            },
-            minHeight: {
-              name: 'min-height',
-              value: 40,
-              unit: StyleValueUnit.px
-            },
-            boxSizing: {
-              name: 'box-sizing',
-              value: 'border-box',
-              unit: StyleValueUnit.none
-            },
-            borderTop: {
-              name: 'border-top',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            borderRight: {
-              name: 'border-right',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            borderBottom: {
-              name: 'border-bottom',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            borderLeft: {
-              name: 'border-left',
-              value: 0,
-              unit: StyleValueUnit.px
-            },
-            position: {
-              name: 'position',
-              value: 'static',
-              unit: StyleValueUnit.none
-            },
-            background: {
-              name: 'background',
-              value: '#fff',
-              unit: StyleValueUnit.none
-            },
-            boxShadow: {
-              name: 'box-shadow',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            transition: {
-              name: 'transition',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            animation: {
-              name: 'animation',
-              value: '',
-              unit: StyleValueUnit.none
-            }
+            ...positionStyle,
+            ...visualEffectStyle,
+            ...borderStyle,
+            ...boxModelStyle,
           }
         },
         children: []
@@ -147,61 +223,11 @@ class SchemaService implements SchemaOperator {
         props: {
           data: '文本控件',
           style: {
-            width: {
-              name: 'width',
-              value: 'auto',
-              unit: StyleValueUnit.none
-            },
-            height: {
-              name: 'height',
-              value: 20,
-              unit: StyleValueUnit.px
-            },
-            fontFamily: {
-              name: 'font-family',
-              value: 'Microsoft YaHei',
-              unit: StyleValueUnit.px,
-            },
-            fontSize: {
-              name: 'font-size',
-              value: 12,
-              unit: StyleValueUnit.px
-            },
-            fontWeight: {
-              name: 'font-weight',
-              value: 600,
-              unit: StyleValueUnit.none
-            },
-            lineHeight: {
-              name: 'line-height',
-              value: 20,
-              unit: StyleValueUnit.px
-            },
-            color: {
-              name: 'color',
-              value: '#000',
-              unit: StyleValueUnit.none
-            },
-            position: {
-              name: 'position',
-              value: 'static',
-              unit: StyleValueUnit.none
-            },
-            boxShadow: {
-              name: 'box-shadow',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            transition: {
-              name: 'transition',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            animation: {
-              name: 'animation',
-              value: '',
-              unit: StyleValueUnit.none
-            }
+            width,
+            height,
+            ...fontStyle,
+            ...positionStyle,
+            ...visualEffectStyle,
           }
         }
       };
@@ -226,26 +252,8 @@ class SchemaService implements SchemaOperator {
               value: 100,
               unit: StyleValueUnit.px
             },
-            position: {
-              name: 'position',
-              value: 'static',
-              unit: StyleValueUnit.none
-            },
-            boxShadow: {
-              name: 'box-shadow',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            transition: {
-              name: 'transition',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            animation: {
-              name: 'animation',
-              value: '',
-              unit: StyleValueUnit.none
-            }
+            ...positionStyle,
+            ...visualEffectStyle,
           }
         }
       };
@@ -267,31 +275,15 @@ class SchemaService implements SchemaOperator {
               value: Layout.block,
               unit: StyleValueUnit.none
             },
+            ...boxModelStyle,
             minHeight: {
               name: 'min-height',
               value: 40,
               unit: StyleValueUnit.px
             },
-            position: {
-              name: 'position',
-              value: 'static',
-              unit: StyleValueUnit.none
-            },
-            boxShadow: {
-              name: 'box-shadow',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            transition: {
-              name: 'transition',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            animation: {
-              name: 'animation',
-              value: '',
-              unit: StyleValueUnit.none
-            }
+            ...borderStyle,
+            ...positionStyle,
+            ...visualEffectStyle,
           }
         },
         children: []
@@ -309,26 +301,8 @@ class SchemaService implements SchemaOperator {
             value: ''
           },
           style: {
-            position: {
-              name: 'position',
-              value: 'static',
-              unit: StyleValueUnit.none
-            },
-            boxShadow: {
-              name: 'box-shadow',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            transition: {
-              name: 'transition',
-              value: '',
-              unit: StyleValueUnit.none
-            },
-            animation: {
-              name: 'animation',
-              value: '',
-              unit: StyleValueUnit.none
-            }
+            ...positionStyle,
+            ...visualEffectStyle,
           }
         }
       };
